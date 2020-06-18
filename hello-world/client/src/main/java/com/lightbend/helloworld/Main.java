8a65250529ff75bc30cc1b3dd98d8d78f89e9d46
Eduard Castro <eduard.cas07@gmail.com>
2020-06-18 12:47:09 -0500

    Add service client

---
+++ hello-world/client/src/main/java/com/lightbend/helloworld/Main.java
@@ -0,0 +1,23 @@
+package com.lightbend.helloworld;
+
+import com.lightbend.lagom.javadsl.client.integration.LagomClientFactory;
+
+import java.net.URI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            LagomClientFactory clientFactory = LagomClientFactory.create("client", LagomClientFactory.class.getClassLoader());
+            HelloWorldService helloWorldService = clientFactory.createClient(HelloWorldService.class, new URI("http://localhost:9000"));
+
+            String result = helloWorldService.helloWorld().invoke().toCompletableFuture().get();
+
+            System.out.println(result);
+
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+}
